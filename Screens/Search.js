187d6c3d96e0f41c8985276afcaf494a0efb5f37
dannertech <danner@danner.tech>
2019-09-09 05:37:10 -0400

    created screens and reconstructed App.js

---
+++ Screens/Search.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+
+export default class Search extends Component {
+    render(){
+        return(
+            <Text>This is the Search page</Text>
+        )
+    }
+}
